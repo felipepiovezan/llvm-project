a83530155aca268e4adc70535a92a2f81de99712
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2022-11-11 17:21:50 -0800

    [MCCAS] Add ref4 to the list of forms that wont dedup

--- llvm/lib/MC/MCCASDebugV1.cpp
+++ llvm/lib/MC/MCCASDebugV1.cpp
@@ -198,6 +198,7 @@ bool mccasformats::v1::doesntDedup(dwarf::Form Form, dwarf::Attribute Attr) {
       FormsToPartition{
           {dwarf::Form::DW_FORM_strp, {}},
           {dwarf::Form::DW_FORM_ref_addr, {}},
+          {dwarf::Form::DW_FORM_ref4, {}},
           {dwarf::Form::DW_FORM_data1,
            {dwarf::Attribute::DW_AT_call_file,
             dwarf::Attribute::DW_AT_decl_file,

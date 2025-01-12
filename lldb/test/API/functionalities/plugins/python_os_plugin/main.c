ce84c8c9583d3cf5089e9258ee40a34515a2e605
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2025-01-12 10:17:12 -0800

    [lldb][test] Simplify test to debug issue

--- lldb/test/API/functionalities/plugins/python_os_plugin/main.c
+++ lldb/test/API/functionalities/plugins/python_os_plugin/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+void foo() {
+    puts("stop here"); // Set breakpoint here
+    puts("more code to step out from");
+}
+
 int main (int argc, char const *argv[], char const *envp[])
 {
-    puts("stop here"); // Set breakpoint here
+    foo();
     return 0;
 }

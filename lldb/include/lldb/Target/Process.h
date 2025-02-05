a8e234b22a7589c0c4b62940c5933bbd0027e50f
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2025-02-05 10:00:30 -0800

    remove customizations

--- lldb/include/lldb/Target/Process.h
+++ lldb/include/lldb/Target/Process.h
@@ -2382,10 +2382,6 @@ void PruneThreadPlans();
 
   void SynchronizeThreadPlans();
 
-  /// From the detached thread plan stacks, find the first stack that explains
-  /// the stop represented by the thread and the event.
-  lldb::ThreadPlanSP FindDetachedPlanExplainingStop(Thread &thread, Event *event_ptr);
-
   /// Helper function for FindDetachedPlanExplainingStop. Exists only to be
   /// marked as a C++ friend of `ThreadPlan`.
   lldb::ThreadPlanSP DoesStackExplainStopNoLock(ThreadPlanStack &stack,

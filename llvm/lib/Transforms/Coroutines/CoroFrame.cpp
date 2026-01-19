5f580f7560c75362a965a8cb3a71d5d4cbae1156
Felipe de Azevedo Piovezan <fpiovezan@apple.com>
2026-01-19 11:48:54 +0000

    save frame ptr in alloca in entry funclets

--- llvm/lib/Transforms/Coroutines/CoroFrame.cpp
+++ llvm/lib/Transforms/Coroutines/CoroFrame.cpp
@@ -1946,21 +1946,22 @@ salvageDebugInfoImpl(SmallDenseMap<Argument *, AllocaInst *, 4> &ArgToAllocaMap,
     return std::nullopt;
 
   auto *StorageAsArg = dyn_cast<Argument>(Storage);
-  const bool IsSwiftAsyncArg =
-      StorageAsArg && StorageAsArg->hasAttribute(Attribute::SwiftAsync);
 
-  // Swift async arguments are described by an entry value of the ABI-defined
-  // register containing the coroutine context.
+  // Use an EntryValue when requested (UseEntryValue) for swift async Arguments.
   // Entry values in variadic expressions are not supported.
-  if (IsSwiftAsyncArg && UseEntryValue && !Expr->isEntryValue() &&
-      Expr->isSingleLocationExpression())
+  const bool WillUseEntryValue =
+      UseEntryValue && StorageAsArg &&
+      StorageAsArg->hasAttribute(Attribute::SwiftAsync) &&
+      !Expr->isEntryValue() && Expr->isSingleLocationExpression();
+
+  if (WillUseEntryValue)
     Expr = DIExpression::prepend(Expr, DIExpression::EntryValue);
 
   // If the coroutine frame is an Argument, store it in an alloca to improve
   // its availability (e.g. registers may be clobbered).
   // Avoid this if the value is guaranteed to be available through other means
   // (e.g. swift ABI guarantees).
-  if (StorageAsArg && !IsSwiftAsyncArg) {
+  if (StorageAsArg && !WillUseEntryValue) {
     auto &Cached = ArgToAllocaMap[StorageAsArg];
     if (!Cached) {
       Cached = Builder.CreateAlloca(Storage->getType(), 0, nullptr,
